e38f727aa1e64ea27f0650669ce12fdced491d0b
loic boucha <loic.boucha@gmail.com>
2013-05-14 16:48:26 +0200

    debug ralphe

--- application/controller/Task_controller.php
+++ application/controller/Task_controller.php
@@ -1,9 +1,13 @@
 <?php
-class Task_controller implements Controller_onterface {
+class Task_controller implements Controller_interface {
 
+    public function defaultAction()
+    {
+        $this->liste();
+    }
     public function liste(){
         //die("hello");
-        $Fdao = new DAO_factory(Application::getConfig("DB"));
+        $Fdao = new DAO_factory(Application::getConfigDb());
         $Cdao = $Fdao->getTaskDao();
         
         return new View_vue(array(
