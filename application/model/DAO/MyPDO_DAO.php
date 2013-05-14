10d6a1190d097c30ba55a120732824527d29a2c1
Rodolphe <Rodolphe_Cornet@live.be>
2013-05-14 14:03:56 +0200

    Création du fichier: MyPDO_DAO (Rod)

---
+++ application/model/DAO/MyPDO_DAO.php
@@ -0,0 +1,15 @@
+
+<?php
+
+class MyPDO_DAO extends PDO{
+
+        public function __construct($config){ 
+        parent::__construct("mysql:host=".$config["HOST"].";dbname=".$config["DB"], $config["USER"] , $config["PWD"], 
+                array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, 
+                    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\'' ));
+        }
+    
+}
+
+
+?>
