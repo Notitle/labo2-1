4cd42557e62dea53e623932d01afba1cbfbe7442
loic boucha <loic.boucha@gmail.com>
2013-05-14 11:50:03 +0200

    Dossiers + squelette app

---
+++ application/config.php
@@ -0,0 +1,28 @@
+<?php
+$config = array(
+    "PATH" => array(
+        "base" => __DIR__,
+        "model" => "model",
+        "vue" => "view",
+        "controller" => "controller",
+        "exception" => "model/Exceptions",
+        "interface" => "model/Interfaces",
+        "validateur" => "model/Validateurs",
+        "utils" => "utils",
+        "DAO" => "model/DAO",
+        "metier" => "model/Metiers",
+        "factory" => "model/Factories",
+        "domain" =>"labo2.nylgraphics.be"
+    ),
+    "DB" => array(
+        "HOST" => "localhost",
+        "USER" => "labo2",
+        "PWD" => "php1234",
+        "DB" => "labo2"
+    ),
+    "MVC" => array(
+        "default_controller" =>"Connexion",
+        "default_action" => "login"
+    )
+);
+?>
