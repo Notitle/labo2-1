aa74cf689c389cb94957ce72f6f4fabc5d5831a4
Jerome Leboutte <jerome.leboutte@gmail.com>
2013-05-14 13:55:40 +0200

    Validateur OutOfRange

---
+++ application/model/Validateurs/OutOfRange_validateur.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Description of Length_validateur
+ *
+ * @author internet07
+ */
+class Length_validateur implements Validateur_interface{
+    private $min=3;
+    private $max=20;
+    /**
+     * fonction construct qui ... ben construit ? wouaip,wouaip elle construit des trucs
+     * @param int $max valeur max dans un champs
+     * @param int $min valeur min dans un champs
+     */
+    public function __construct($min,$max) {
+        $this->min=$min;
+        $this->max=$max;
+    }
+    
+        /**
+     * fonction construct qui valide le c
+     * @param int $max valeur max dans un champs
+     * @param int $min valeur min dans un champs
+     * @throws OutOfRange_validateur_exception
+     */
+    
+    public function valide($valeur) {
+        if ($this->min > $valeur ||$this->max<$valeur) {
+            throw new OutOfRange_validateur_exception("erreur de outofrange ");
+        }        
+   } 
+}
+?>
