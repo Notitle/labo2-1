4bcc030c6c67ea3d0476291ca8fcc64a4ff9eb4f
Rodolphe <Rodolphe_Cornet@live.be>
2013-05-14 16:09:38 +0200

    Mise à jour (Rod)

--- application/controller/Task_controller.php
+++ application/controller/Task_controller.php
@@ -1,12 +1,14 @@
 <?php
-class Task_controller {
+class Task_controller implements Controller_onterface {
+
     public function liste(){
-        die("hello");
-        /*$Fdao = new Dao_factory(Application::getConfig("DB"));
-        $Cdao = $Fdao->getCandidatsDAO();
-        return new vue_vue(array(
-            "Liste_Candidats"=>$Cdao->getListCandidats())
-        );*/
+        //die("hello");
+        $Fdao = new DAO_factory(Application::getConfig("DB"));
+        $Cdao = $Fdao->getTaskDao();
+        
+        return new View(array(
+            "Liste_Taches"=>$Cdao->getTaskList())
+        );
     }
 }
 
