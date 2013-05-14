062458c32b0087545b8a3d40a3d279a5d5742ff0
Rodolphe <Rodolphe_Cornet@live.be>
2013-05-14 15:03:02 +0200

    Création du controller pour les task

---
+++ application/controller/Task_controller.php
@@ -0,0 +1,14 @@
+<?php
+class Task_controller {
+    public function liste(){
+        die("hello");
+        /*$Fdao = new Dao_factory(Application::getConfig("DB"));
+        $Cdao = $Fdao->getCandidatsDAO();
+        return new vue_vue(array(
+            "Liste_Candidats"=>$Cdao->getListCandidats())
+        );*/
+    }
+}
+
+?>
+    
