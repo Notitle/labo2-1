6ad9c1bac2ac78dafa1a611f2dcc6f216d4fb8b3
loic boucha <loic.boucha@gmail.com>
2013-05-15 11:32:34 +0200

    DAOFactory en static dans Application

--- application/controller/Task_controller.php
+++ application/controller/Task_controller.php
@@ -1,24 +1,29 @@
 <?php
-class Task_controller implements Controller_interface {
+
+class Task_controller implements Controller_interface
+{
 
     public function defaultAction()
     {
         $this->liste();
     }
-    public function liste(){
+
+    public function liste()
+    {
         //die("hello");
-        $Fdao = new DAO_factory(Application::getConfigDb());
-        $Cdao = $Fdao->getTaskDao();
-        $vue =  new View_vue(array(
-            "Liste_Taches"=>$Cdao->getTaskList())
+        $Cdao = Application::getDAOFactory()->getTaskDao();
+
+        $vue = new View_vue(array(
+            "Liste_Taches" => $Cdao->getTaskList())
         );
         $vue->display($this, 'liste');
     }
-    
-    public function __toString() {
+
+    public function __toString()
+    {
         return 'Task';
     }
-}
 
+}
 ?>
     
