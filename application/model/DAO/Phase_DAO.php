93dae91ba0af228a5c7f362aac787b1fc0063391
Sarah Vanesse <sarahvanesse@hotmail.com>
2013-05-15 14:29:48 +0200

    d

--- application/model/DAO/Phase_DAO.php
+++ application/model/DAO/Phase_DAO.php
@@ -6,6 +6,7 @@
  * @author sarah
  */
 class Phase_DAO {
+
     /**
      *
      * @var tableau des phases $phase_liste
@@ -21,19 +22,35 @@ class Phase_DAO {
      * requete de sélection des phases
      * @return tableau des phases
      */
-    public function getPhaseList(){
-        $sql='SELECT * FROM phase';
-        $stmt=$this->pdo->prepare($sql);
-        $stmt=execute();
+    public function getPhaseList() {
+        $sql = 'SELECT * FROM phase';
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute();
         $stmt->setFetchMode(PDO::FETCH_ASSOC);
-        
-        foreach ($stmt as $key =>$row){
-            $this->phase_liste[$row['pha_id']]=new Phase_metier
-                    ($row['pha_id'],$row['pha_name'],$row['pha_project_fk']);
+
+        foreach ($stmt as $key => $row) {
+            $this->phase_liste[$row['pha_id']] = new Phase_metier
+                    ($row['pha_id'], $row['pha_name'], $row['pha_project_fk']);
         }
         return $this->phase_liste;
     }
-    
+
+    public function getPhaseById($id) {
+        if (!isset($this->phase_liste[$id])) {
+
+            $query = '
+                    SELECT *
+                    FROM phase
+                    WHERE pha_id = :a';
+            $stmt = $this->pdo->prepare($query);
+            $stmt->execute(array(":a" => $id));
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $row = $stmt->fetch();
+            $this->phase_liste[$id] = new Phase_metier($row['pha_id'], $row['pha_name'], $row['pha_project_fk']);
+        }
+        return $this->phase_liste[$id];
     }
+
+}
 ?>
 
