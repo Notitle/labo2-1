5232fa3ef4ef6ee79bbcdef671e8e7e2467409ac
loic boucha <loic.boucha@gmail.com>
2013-05-16 14:56:34 +0200

    DEBUGAGE GENERAAAAAAAAAAAAAAAAAAAAl 2.0

--- application/model/DAO/Task_DAO.php
+++ application/model/DAO/Task_DAO.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Classe reprennant categorie_metier
  *
@@ -30,11 +31,12 @@ class Task_DAO
         { // boucle foreach pr parcourir le tableau associatif, utilisation de l'objet instancié à la classe task_metier avec les paramètres correspondants
             if (!isset($this->task_Liste[$row["tas_id"]]))
             {
-                $this->task_Liste[$row["tas_id"]] = new Task_metier($row['tas_id'], $row['tas_description'], $row['tas_creation'], $row['tas_phase_fk']);       
+                $this->task_Liste[$row["tas_id"]] = new Task_metier($row['tas_id'], $row['tas_description'], $row['tas_creation'], $row['tas_phase_fk']);
             }
         }
         return $this->task_Liste; // !! ne pas oublier le return!! ;-)
     }
+
     /**
      * select d'une tache via un id
      * @param type $id
@@ -58,81 +60,99 @@ class Task_DAO
         }
         return $this->task_Liste[$id];
     }
+
     /**
      * modif/ajout d'une tache
      * @param Task_metier $tm
      */
-    public function setTask(Task_metier $tm){
-        if ($tm->id != 0) {
+    public function setTask(Task_metier $tm)
+    {
+        if ($tm->id != 0)
+        {
             $tache = $this->PDO->prepare("UPDATE task SET tas_description = :b, tas_creation = :c, tas_phase_fk = :d WHERE tas_id = :a");
-            $tache->execute(array(':a' => $tm->id, ':b' => $tm->tache,':c' => $tm->date, ':d' => $tm->phase));
+            $tache->execute(array(':a' => $tm->id, ':b' => $tm->tache, ':c' => $tm->date, ':d' => $tm->phase));
             $this->task_liste[$tm->$id] = $tm;
         }
-        
-        else{
+        else
+        {
             $tache = $this->PDO->prepare("INSERT INTO task (tas_description, tas_creation, tas_phase_fk) VALUES (:b, :c, :d) WHERE pha_id = :a");
-            $tache->execute(array(':a' => $tm->id, ':b' => $tm->tache,':c' => $tm->date, ':d' => $tm->phase));
+            $tache->execute(array(':a' => $tm->id, ':b' => $tm->tache, ':c' => $tm->date, ':d' => $tm->phase));
             $this->task_liste[$tm->$id] = $tm;
         }
     }
+
     /**
      * suppression d'une tâche
      * @param type $id
      * @return string
      */
-    public function deleteTask ($id){
-         if(isset ($this->task_Liste[$id])){
-             unset($this->task_Liste[$id]);
-         }
-         else{
-             return "La tâche n'existe pas, Nom d'un chien ! Le capitaine Jérôme a encore bu trop de Whisky !!!!!!!!!!!!!!!!!! !";
-         }
-         $task=$this->PDO->prepare("DELETE FROM task WHERE tas_id=:a" );
-         $task->execute(array(':a'=>$id));
+    public function deleteTask($id)
+    {
+        if (isset($this->task_Liste[$id]))
+        {
+            unset($this->task_Liste[$id]);
+        }
+        else
+        {
+            return "La tâche n'existe pas, Nom d'un chien ! Le capitaine Jérôme a encore bu trop de Whisky !!!!!!!!!!!!!!!!!! !";
+        }
+        $task = $this->PDO->prepare("DELETE FROM task WHERE tas_id=:a");
+        $task->execute(array(':a' => $id));
     }
+
     /**
      * select d'une tache via l'objet phase
      * @param Phase_metier $phase
      * @return type
      */
-    public function getTaskByPhase (Phase_metier $phase){
-        $query='
+    public function getTaskByPhase(Phase_metier $phase)
+    {
+        $query = '
             SELECT tas_id, tas_description, tas_creation, tas_phase_fk
             FROM task
             WHERE tas_phase_fk =:a';
         $result = $this->pdo->prepare($query);
-        $result -> setFetchMode (PDO::FETCH_ASSOC);
-        $result -> execute(array(":a"=>$phase->getId()));
-        $return=array();
-        foreach ($result as $key => $row){
-            if(!isset($this->task_Liste[$row["tas_id"]])){
-                $this->task_Liste[$row["tas_id"]]=new Task_metier($row["tas_id"],$row["tas_description"],$row["tas_creation"],$row["tas_phase_fk"]);
-            }
-            $return[]= $this->task_Liste[$row["tas_id"]];
-        }
-          return $return;
-    }
-   
-    /**A MODIFIER**/
-    public function getTaskByUser(Utilisateur_metier $user){
-        $query='SELECT his_task_fk FROM history WHERE his_user_fk=:a';
-        $result=$this->pdo->prepare($query);
         $result->setFetchMode(PDO::FETCH_ASSOC);
-        $result->execute(array(":a"=>$user->getIdentifiant()));
-        $return=array();
-        foreach ($result as $key => $row){
-            if(!isset($this->his_liste[$row["his_id"]])){
-                $this->his_liste[$row["his_id"]]=new Task_metier($row["tas_id"],$row["tas_description"],$row["tas_creation"],$row["tas_phase_fk"]);
+        $result->execute(array(":a" => $phase->getId()));
+        $return = array();
+        foreach ($result as $key => $row)
+        {
+            if (!isset($this->task_Liste[$row["tas_id"]]))
+            {
+                $this->task_Liste[$row["tas_id"]] = new Task_metier($row["tas_id"], $row["tas_description"], $row["tas_creation"], $row["tas_phase_fk"]);
             }
-            $return[]= $this->task_Liste[$row["tas_id"]];
+            $return[] = $this->task_Liste[$row["tas_id"]];
         }
-          return $return;
-            
+        return $return;
+    }
+
+    
+    /**
+     * T'as qu'as lire le nom de la fonction, c'est clair. 
+     * @param Utilisateur_metier $user
+     * @return Array
+     */
+    public function getTasksByUser(Utilisateur_metier $user)
+    {
+        $query = 'SELECT his_task_fk FROM history WHERE his_user_fk=:a';
+        $result = $this->pdo->prepare($query);
+        $result->setFetchMode(PDO::FETCH_ASSOC);
+        $result->execute(array(":a" => $user->getIdentifiant()));
+        $return = array();
+        foreach ($result as $key => $row)
+        {
+            $taskID = $row['his_task_fk'];
+
+            if (!isset($this->task_Liste[$taskID]))
+            {
+                $this->task_Liste[$taskID] = $this->getTaskById($taskID);
+            }
+            $return[] = $this->task_Liste[$taskID];
         }
- 
+        return $return;
+    }
 
 }
-
 ?>
 
- 	
+
