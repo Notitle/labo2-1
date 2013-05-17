0cb69aedd493caaeb215b207f6b4b60536aead9f
Jerome Leboutte <jerome.leboutte@gmail.com>
2013-05-17 13:49:20 +0200

    select/option

---
+++ application/model/Formulaires/FieldSelect_form.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Description of FieldSelect_formµ
+ *
+ * @author internet07
+ */
+class FieldSelect_form extends Field_form {
+
+    private $options;
+    
+
+    function __construct($name, $options) {
+        parent::__construct($name, $options, "select");
+        $this->options = $options;
+    }
+
+    public function getOption_liste() {
+        return $this->options;
+    }
+
+    public function setOption_liste($option_liste) {
+        $this->options = $option_liste;
+    }
+}
+
+?>
