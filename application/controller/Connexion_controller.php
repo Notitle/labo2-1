db92dac89c360d937d1b7782967a60796976cb45
Rodolphe <Rodolphe_Cornet@live.be>
2013-05-17 13:52:42 +0200

    Je n'ai JAMAIS touché au projet! EXEPTÉ cette fois-ci...

--- application/controller/Connexion_controller.php
+++ application/controller/Connexion_controller.php
@@ -20,8 +20,20 @@ class Connexion_controller implements Controller_interface
 
    public function login()
     {
-        $vue = new View_vue(array("form" => new Generic_form("formulaire", "", Generic_form::METHOD_POST)));
+       
+       $form = new Generic_form("connect","",Generic_form::METHOD_POST);
+       $form->addField(new Field_form("user", "", "text","Nom d'utilisateur   "));
+       $form->addField(new Field_form("pdw", "", "password", "Mot de passe "));
+       $form->addField(new Field_form("submit", "Envoyer", "submit"));
+       
+        $vue = new View_vue(array("form" => $form));
         $vue->display($this, "login");
+    }
+    
+    public function addTask(){
+        /*
+         * 
+         */
     }
 
 }
